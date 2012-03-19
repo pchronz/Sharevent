9b84da02e500c9d730234e64faaf6929f6cf38bd
David <davidkimont@gmail.com>
2012-03-19 17:21:53 +0100

    bootstrap2 added

--- ShareventImpl/Sharevent/grails-app/conf/ApplicationResources.groovy
+++ ShareventImpl/Sharevent/grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,29 @@
+def isDev = grails.util.GrailsUtil.isDevelopmentEnv()
+
 modules = {
-    application {
-        resource url:'js/application.js'
-    }
+	application {
+		resource url:'js/application.js'
+	}
+
+	'bootstrap-responsive' {
+		resource url:[dir:'bootstrap/css', file:"bootstrap-responsive.${isDev?'css':'min.css'}"], disposition: 'head', exclude:'minify'	
+	}
+
+	'bootstrap-css' {
+		resource url:[dir:'bootstrap/css', file:"bootstrap.${isDev?'css':'min.css'}"], disposition: 'head', exclude:'minify'	
+	}
+
+	'bootstrap-js' {
+		resource url:[dir:'bootstrap/js', file:"bootstrap.${isDev?'js':'min.js'}"], disposition: 'head', exclude:'minify'	
+	}
+
+	bootstrap {
+		dependsOn 'bootstrap-responsive'
+		dependsOn 'bootstrap-css'
+		dependsOn 'bootstrap-js'	
+	}
+	
+	
+	
+	
 }
